a8961078c163331fd3a9e4a22279890e437055fc
Rzeznik <ironmichal92@gmail.com>
2016-01-24 20:19:21 +0100

    Network first interpretation

---
+++ QueryNetwork/Network.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryNetwork
+{
+    class Network
+    {
+        private List<Node> listOfNodes; // Lista systemów w sieci
+
+    }
+}
+
+/*
+ * Do sieci wchodzi nowe zgłoszenie o klasie K.
+ * Zostaje przydzielone do systemu początkowego We (sztuczny lub wybrany z dostępnych).
+ * Zwiększona zostaje aktualna liczba zgłoszeń w danym systemie: +1.
+ * Jeśli liczba aktualnych zgłoszeń w tym systemie jest mniejsza od ilości stacji obsługi -> zaczynamy obsługę.
+ * Jeśli liczba aktualnych zgłoszeń w tym systemie jest większa od ilości stacji obsługi,
+ *      ale mniejsza od sumy ilości stacji obsługi i miejsc w kolejce -> czeka.
+ * Jeśli liczba aktualnych zgłoszeń w tym systemie jest większa od sumy ilości stacji obsługi i miejsc w kolejce
+ *      -> utrata zgłoszenia (opcjonalne, możemy nie brać pod uwagę takich systemów).
+ * W momencie kiedy upłynie czas obsługi danego zgłoszenia, wybieramy z listy następny mozliwy system S
+ *      (na podstawie algorytmu optymalizacyjnego) i powtarzamy dla niego procedurę.
+ * Zgłoszenie wychodzi z sieci po dotarciu do systemu końcowego Wy.
+ * 
+ * Zakładamy brak priorytetów, niecierpliwych klientów i indywidualnej obsługi.
+*/
---
+++ QueryNetwork/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryNetwork
+{
+    class Node
+    {
+        private int id; // Numer systemu
+        private int systemMaxSize; // Rozmiar systemu (-1 dla nieskończoności)
+        private int queueMaxSize; // Rozmiar kolejki (-1 dla nieskończoności)
+        private int itemCount; // Aktualna liczba zgłoszeń
+        private KeyValuePair<int, double> operationTime; // Czas obsługi dla danej klasy zgłoszenia
+        private List<Link> listOfLinks; // Lista połączeń wychodzących z systemu
+
+    }
+}
