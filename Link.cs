309c724a51602e21e60375f4baa78922ccfa31b2
Rzeznik <ironmichal92@gmail.com>
2016-01-24 19:38:18 +0100

    First commit

---
+++ Link.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueuingNetwork
+{
+    class Link
+    {
+        private int beginningNode; // System początkowy
+        private int endNode; // System docelowy
+        private int entryClass; // Klasa zgłoszenia wejściowa
+        private int exitClass; // Klasa zgłoszenia wyjściowa
+
+    }
+}
