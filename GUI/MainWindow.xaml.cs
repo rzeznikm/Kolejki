5f120037b3967c7f794939dd668f9b63274b047d
Paweł Wojnarowski <pawelswojnarowski@gmail.com>
2016-02-01 23:16:11 +0100

    Refactor Main Window.

--- GUI/MainWindow.xaml.cs
+++ GUI/MainWindow.xaml.cs
@@ -60,8 +60,8 @@ namespace GUI
             (App.Current as App).displaymodelList = new List<DisplayModel>();
 
             //----
-            (App.Current as App).displaymodelList.Add(new DisplayModel() { id = 0, serviceChannelsNumber = -1, queueMaxSize = -1, checkoutTime = 0, systems = "1,2" });
-            (App.Current as App).displaymodelList.Add(new DisplayModel() { id = 1, serviceChannelsNumber = -1, queueMaxSize = -1, checkoutTime = 0, systems = "0" });
+            //(App.Current as App).displaymodelList.Add(new DisplayModel() { id = 0, serviceChannelsNumber = -1, queueMaxSize = -1, checkoutTime = 0, systems = "1,2" });
+            //(App.Current as App).displaymodelList.Add(new DisplayModel() { id = 1, serviceChannelsNumber = -1, queueMaxSize = -1, checkoutTime = 0, systems = "0" });
             //----
             //---- TESTOWE 
             //(App.Current as App).displaymodelList.Add(new DisplayModel() { id = 0, serviceChannelsNumber = -1, queueMaxSize = -1, checkoutTime = 0, systems = "1,2" });
@@ -76,6 +76,15 @@ namespace GUI
 
         private void BT_Run_Click(object sender, RoutedEventArgs e)
         {
+            nodes = new List<Node>();
+
+            foreach (var model in (App.Current as App).displaymodelList)
+            {
+                Node node = new Node(model.id, model.serviceChannelsNumber, model.queueMaxSize, model.checkoutTime, StringToIntList(model.systems));
+                nodes.Add(node);
+            }
+            (App.Current as App).network = new Network(nodes);
+
             (App.Current as App).percentage = percentLocal;
             Window1 win1 = new Window1();
             win1.Show();         
@@ -92,18 +101,6 @@ namespace GUI
             return ret;
         }
 
-        private void BT_Confirm_Click(object sender, RoutedEventArgs e)
-        {
-            nodes = new List<Node>();
-
-            foreach (var model in (App.Current as App).displaymodelList)
-            {
-                Node node = new Node(model.id, model.serviceChannelsNumber, model.queueMaxSize, model.checkoutTime, StringToIntList(model.systems));
-                nodes.Add(node);
-            }
-            (App.Current as App).network = new Network(nodes);
-        }
-
         private void NodeList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
 
@@ -114,5 +111,25 @@ namespace GUI
             percentLocal = (int) Slider1.Value;
             TXB_5.Text = percentLocal.ToString();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            (App.Current as App).displaymodelList.Add(new DisplayModel() { id = 0, serviceChannelsNumber = -1, queueMaxSize = -1, checkoutTime = 0, systems = "1,2" });
+            (App.Current as App).displaymodelList.Add(new DisplayModel() { id = 1, serviceChannelsNumber = 2, queueMaxSize = 4, checkoutTime = 3, systems = "3" });
+            (App.Current as App).displaymodelList.Add(new DisplayModel() { id = 2, serviceChannelsNumber = 1, queueMaxSize = 4, checkoutTime = 4, systems = "3" });
+            (App.Current as App).displaymodelList.Add(new DisplayModel() { id = 3, serviceChannelsNumber = -1, queueMaxSize = -1, checkoutTime = 0, systems = "0" });
+
+            NodeList.ItemsSource = null;
+            NodeList.ItemsSource = (App.Current as App).displaymodelList;              
+        }
+
+        private void BT_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            (App.Current as App).displaymodelList = new List<DisplayModel>();
+            NodeList.ItemsSource = null;
+            NodeList.ItemsSource = (App.Current as App).displaymodelList; 
+        }
+
+
     }
 }
